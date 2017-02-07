7ba2a326d20341072cea010d1183e6371380fa8b
Prameet31 <prameet31o@gmail.com>
2017-02-07 19:39:41 +0530

    Added Element on float button

--- app/src/main/java/com/greedygame/listofbollywoodactor/MainActivity.java
+++ app/src/main/java/com/greedygame/listofbollywoodactor/MainActivity.java
@@ -1,39 +1,72 @@
 package com.greedygame.listofbollywoodactor;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    private FloatingActionButton fab;
+    ArrayList<String> Toppaidactors = new ArrayList<String>();{
+
+        Toppaidactors.add("Aamir");
+        Toppaidactors.add("Salman");
+        Toppaidactors.add("Shahrukh");
+        Toppaidactors.add("Ajay");
+        Toppaidactors.add("Akshay");
+        Toppaidactors.add("Ranveer");
+        Toppaidactors.add("Siddhartha");
+        Toppaidactors.add("Varun");
+        Toppaidactors.add("Arjun Kapoor");
+    }
+    ArrayList<String> Actress = new ArrayList<String>();{
+            Actress.add("Kareena");
+            Actress.add("Katrina");
+            Actress.add("Deepika");
+            Actress.add("Sonam");
+            Actress.add("Sonakshi");
+            Actress.add("Alia");
+    }
+    private ArrayAdapter<String> adapter;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.listview);
-        String[] values = new String[] { "Amitabh Bachhan",
-                "Salman Khan",
-                "Shah Rukh Khan",
-                "Akshay Kumar",
-                "Ajay Devgan",
-                "Ranvir Kapoor",
-                "Arjun Kapoor",
-                "Siddhartha Malhotra",
-                "Varun Dhawan",
-                "Saif Ali Khan",
-                "Hritik Roushan",
-                "Tiger Shroff",
-                "Sushant Singh Rajput",
-                "Dharmendra",
-                "Rajesh Khanna"
-        };
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get a random number from the size of Actress
+                int id = new Random().nextInt(Actress.size());
+                // Get the element from Actress
+                String random = Actress.get(id);
+                // Add random string into Toppaidactors
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+                if(Toppaidactors.contains(random)) {
+                    Toast.makeText(MainActivity.this, "Already added",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toppaidactors.add(0, random);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+        listView = (ListView) findViewById(R.id.listview);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, Toppaidactors);
 
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -47,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
 
 
 

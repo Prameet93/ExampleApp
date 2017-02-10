7bdaa362818af65b16232308e0c32e74ed2dd766
Prameet31 <prameet31o@gmail.com>
2017-02-10 19:29:30 +0530

    AsyncTask Demo

--- app/src/main/java/com/greedygame/listofbollywoodactor/Main2Activity.java
+++ app/src/main/java/com/greedygame/listofbollywoodactor/Main2Activity.java
@@ -1,6 +1,6 @@
 package com.greedygame.listofbollywoodactor;
 
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,56 +8,24 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import static android.R.id.list;
-
 public class Main2Activity extends AppCompatActivity {
+    private ArrayAdapter<Integer> adapter;
+    Context context;
+    ArrayList<Integer> innerArray = new ArrayList<Integer>();
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_main2);
-    }
-}
-public class AsyncTaskRunner extends AsyncTask<String, String, String> {
-
-    private String resp;
-    ProgressDialog progressDialog;
-    ListView listView;
-    private ArrayAdapter<Integer> adapter;
-    ArrayList<Integer> innerArray = new ArrayList<Integer>();
-
-    @Override
-    protected String doInBackground(String... params) {
-        publishProgress("Sleeping..."); // Calls onProgressUpdate()
-        try {
-            int time = Integer.parseInt(params[0])*1000;
-            for (int i = 1; i <= 10000; i++){
-                innerArray.add(i);
-            }
-
-            Thread.sleep(time);
-            resp = "Slept for " + params[0] + " seconds";
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            resp = e.getMessage();
-        } catch (Exception e) {
-            e.printStackTrace();
-            resp = e.getMessage();
-        }
-        return resp;
-    }
-    @Override
-    protected void onPostExecute(String result) {
-        // execution of result of Long time consuming operation
-
-        ArrayList<Integer> innerArray = new ArrayList<Integer>();
-        ArrayAdapter<Integer> adapter;
         listView = (ListView) findViewById(R.id.listview);
-        adapter = new ArrayAdapter<Integer>(this,
+        adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, innerArray);
-
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -65,25 +33,35 @@ public class AsyncTaskRunner extends AsyncTask<String, String, String> {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 int itemPosition = position;
-                String itemValue = (String) listView.getItemAtPosition(position);
+                int itemValue = (int) listView.getItemAtPosition(position);
             }
         });
-
+        AsyncTaskRunner task = new AsyncTaskRunner();
+        task.execute();
     }
 
+    public class AsyncTaskRunner extends AsyncTask<String, String, String> {
 
-    @Override
-    protected void onPreExecute() {
-        progressDialog = ProgressDialog.show(Main2Activity.this,
-                "ProgressDialog",
-                "Wait for "+time.getText().toString()+ " seconds");
-    }
+        @Override
+        protected String doInBackground(String... params) {
+            for (int i = 1; i <= 10000; i++) {
+                innerArray.add(i);
+            }
+            return null;
+        }
 
+        @Override
+        protected void onPostExecute(String result) {
+            adapter.notifyDataSetChanged();
 
-    @Override
-    protected void onProgressUpdate(String... text) {
-        finalResult.setText(text[0]);
+        }
 
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(Main2Activity.this, "Pre execute",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
-}
+
+
